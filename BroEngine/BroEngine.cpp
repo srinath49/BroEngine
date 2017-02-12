fff740c32c277f30fd972349d0d4fab4e16f3c8e
Srinath Upadhyayula <supadhyayula@smu.edu>
2017-02-12 14:13:38 -0500

    Adding Glfw

--- BroEngine/BroEngine.cpp
+++ BroEngine/BroEngine.cpp
@@ -1,32 +1,42 @@
-#define WIN32_LEAN_AND_MEAN
-
 #include <stdio.h>
 #include "EngineCode/Math/IntVector2.hpp"
 #include "EngineCode/Math/FloatVector2.hpp"
+#include "glfw3.h"
 
+//---------------------------------------------------------------------------------------------------
 #ifndef UNUSED
 #define UNUSED (void)
 #endif // !UNUSED
 
+//---------------------------------------------------------------------------------------------------
+GLFWwindow*		g_window;
 
+//---------------------------------------------------------------------------------------------------
+void InitializeWindow();
+void UninitializeWindow();
+
+//---------------------------------------------------------------------------------------------------
 int main()
 {
-	IntVector2 vec1 = IntVector2(1 , 1);
-	IntVector2 vec2 = IntVector2(2 , 2);
-	IntVector2* result;
-	//FloatVector2 fVec2;
-	//FloatVector2* fvec2 = new FloatVector2(1 , 2);
-	//printf("%f, %f\n", fvec2->x , fvec2->y);
-	//printf("%d, %d\n", vec2.x, vec2.y);
-	//fVec2 /= 4;
-
-	//result = &(*vec1 + *vec2);
-	vec1 = vec2;
-
-	printf("%d , %d\n" , vec1.x , vec1.y);
-
-	//Test all your functions here
-	
+	InitializeWindow();
 	getchar();
+	UninitializeWindow();
 	return 0;
+}
+
+//---------------------------------------------------------------------------------------------------
+void InitializeWindow()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	g_window = glfwCreateWindow(800, 600, "Bob Window", nullptr, nullptr);
+}
+
+//---------------------------------------------------------------------------------------------------
+void UninitializeWindow()
+{
+	glfwDestroyWindow(g_window);
+
+	glfwTerminate();
 }
