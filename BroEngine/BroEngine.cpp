7e6100355ccdb43bd6b3af46a04ff1bb7b3721c0
Srinath Upadhyayula <supadhyayula@smu.edu>
2017-03-04 11:54:54 -0500

    Added output console for debugging

--- BroEngine/BroEngine.cpp
+++ BroEngine/BroEngine.cpp
@@ -1,9 +1,14 @@
 #define WIN32_LEAN_AND_MEAN
 #include "Windows.h"
 #include <stdio.h>
+#include "Main/Win32OpenGLApp.hpp"
+#include <io.h>
+#include <fcntl.h>
+#include <cassert>
+#include <crtdbg.h>
+#include <stdlib.h>
 #include <stdexcept>
 #include <iostream>
-#include "Main/Win32OpenGLApp.hpp"
 
 //---------------------------------------------------------------------------------------------------
 #ifndef UNUSED
@@ -11,9 +16,27 @@
 #endif // !UNUSED
 
 //---------------------------------------------------------------------------------------------------
+void StartWindowsConsole()
+{
+	//_CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF);
+	// This code was take from the internet. 
+	AllocConsole();
+	HANDLE stdHandle;
+	int hConsole;
+	FILE* fp;
+	stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	hConsole = _open_osfhandle((long)stdHandle, _O_TEXT);
+	fp = _fdopen(hConsole, "w");
+
+	freopen_s(&fp, "CONOUT$", "w", stdout);
+}
+
+//---------------------------------------------------------------------------------------------------
 int WINAPI WinMain(HINSTANCE applicationInstanceHandle, HINSTANCE, LPSTR, int )
 {
 	int returnValue = EXIT_SUCCESS;
+	StartWindowsConsole();
+
 	BaseApp* app = new Win32OpenGLApp(applicationInstanceHandle);
 	
 	try
